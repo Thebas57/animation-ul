3b9dfb0a23e2b2f5be5ad3ce88940dd3ed4b60ec
Thebas57 <theohelf@gmail.com>
2023-05-14 19:00:57 +0200

    animation et image menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { useEffect } from "react";
+
 function App() {
+  const intersectionFunction = () => {
+    console.log("test")
+    const observerOptions = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5, // 0.5 signifie que la moitié de la div est visible pour déclencher l'intersection
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          console.log("rt");
+          observer.unobserve(entry.target); // Arrêter l'observation une fois que la div est visible
+        }
+      });
+    }, observerOptions);
+
+    const divToObserve = document.querySelector(".line");
+    observer.observe(divToObserve);
+  };
+  useEffect(() => {
+    intersectionFunction();
+  }, []);
   return (
     <div className="App">
       <ul>
         <li className="line">
+          <div className="img-back"></div>
           <div className="first">
             <span>01</span>
             <h3>Chasing Gold</h3>
@@ -13,6 +40,7 @@ function App() {
           </div>
         </li>
         <li className="line">
+          <div className="img-back"></div>
           <div className="first">
             <span>02</span>
             <h3>Be More Human</h3>
@@ -23,6 +51,7 @@ function App() {
           </div>
         </li>
         <li className="line">
+          <div className="img-back"></div>
           <div className="first">
             <span>03</span>
             <h3>Legacy Summit</h3>
@@ -33,6 +62,7 @@ function App() {
           </div>
         </li>
         <li className="line">
+          <div className="img-back"></div>
           <div className="first">
             <span>04</span>
             <h3>Creative We Stand</h3>
@@ -43,6 +73,7 @@ function App() {
           </div>
         </li>
         <li className="line">
+          <div className="img-back"></div>
           <div className="first">
             <span>01</span>
             <h3>Chasing Gold</h3>
