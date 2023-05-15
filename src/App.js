d927d0b72007959875f7c3abaf05f2dab063c9ca
Thebas57 <theohelf@gmail.com>
2023-05-15 14:07:15 +0200

    fin de l'animation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
 import { useEffect } from "react";
 
 function App() {
+  // Pour le curseur
+  document.addEventListener("mousemove", function (event) {
+    let cursor = document.querySelector(".cursor");
+    cursor.style.left = event.clientX + "px";
+    cursor.style.top = event.clientY + "px";
+  });
+  // Quand une div s'affiche, on lui ajoute la classe visible
   const intersectionFunction = () => {
-    console.log("test")
+    console.log("test");
     const observerOptions = {
       root: null,
-      rootMargin: "0px",
-      threshold: 0.5, // 0.5 signifie que la moitié de la div est visible pour déclencher l'intersection
+      rootMargin: "0px 0px 50px 0px",
+      threshold: 1, // 0.5 signifie que la moitié de la div est visible pour déclencher l'intersection
     };
 
     const observer = new IntersectionObserver((entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add("visible");
-          console.log("rt");
-          observer.unobserve(entry.target); // Arrêter l'observation une fois que la div est visible
         }
       });
     }, observerOptions);
 
-    const divToObserve = document.querySelector(".line");
-    observer.observe(divToObserve);
+    const divsToObserve = document.querySelectorAll(".line");
+    divsToObserve.forEach((div) => {
+      observer.observe(div);
+    });
   };
   useEffect(() => {
     intersectionFunction();
   }, []);
   return (
     <div className="App">
+      <div className="cursor"></div>
+      <div className="header">
+        <span>Théo HELF</span>
+        <h3>Animation Menu</h3>
+        <span>2023</span>
+      </div>
+      <div className="scroll">
+        <span>SCROLL</span>
+      </div>
       <ul>
         <li className="line">
           <div className="img-back"></div>
